7412d1422d5aad0876bb738f1eb6114ea11a068f
makoto0119 <makoto0119@gmail.com>
2022-07-17 00:09:16 +0900

    手直し更新

--- outlook2list.py
+++ outlook2list.py
@@ -1,7 +1,7 @@
 import win32com.client
 import datetime
 import pyperclip
-import win32timezone
+import time
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
@@ -19,6 +19,7 @@ today_date = datetime.date.today() # 今日だけ
 # restrict appointments to specified range
 calendar = calendar.Restrict("[Start] >= '" + str(today_date) +
                              "' AND [END] <= '" + str(today_date + datetime.timedelta(days=1)) + "'")
+
 for item in calendar:
     if today_date == item.start.date():
         select_items.append(item)
@@ -26,19 +27,26 @@ for item in calendar:
         break 
 
 text =""
+
 # 抜き出した予定の詳細を表示
 for select_item in select_items:
-    print("件名：", select_item.subject)
+    if (select_item.subject.startswith('Canceled:') or 
+        select_item.subject.startswith('キャンセル済み:') or 
+        select_item.subject.startswith('**') or 
+        select_item.subject =='お昼休み') :
+        continue
+    item = select_item.start.time().strftime('%H:%M ') + select_item.subject
+    print("件名", item)
+
+#    print("件名：", select_item.subject)
 #    print("日時：", select_item.start.date())
 #    print("場所：", select_item.location)
 #    print("開始時刻：", select_item.start.time())
 #    print("終了時刻：", select_item.end)
 #    print("本文：", select_item.body)
-#    print("----")
 
-#    text = text + select_item.subject + '\r\n'
-    text = text + select_item.subject + '\r'
+    text = text + '　' + item + '\r'
 
 pyperclip.copy(text)
 print("クリップボードにコピーしました")
-input("")
+time.sleep(30)
